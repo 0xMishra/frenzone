83e0742e46e6523e5f93751eef7fcd73f0c773df
kundan mishra <mishrakundan073@gmail.com>
2022-07-09 19:09:23 +0530

    initialized client side application

---
+++ client/src/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Frenzone.json";
+const CONTRACT_ADDRESS = "0x4CCD6e2406aa307206cB67d90df9cD72760B7Fa6";
+const CONTRACT_ABI = abi.abi;
+
+export { CONTRACT_ABI, CONTRACT_ADDRESS };
